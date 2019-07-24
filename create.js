e831351b6b992c932f601f78ba5b68d4ad807734
Fred Policarpo <frederico.policarpo@gmail.com>
2019-07-23 22:37:25 -0300

    Create lambda function to add note
    Configure API path to the created lambda function

---
+++ create.js
@@ -0,0 +1,35 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: "notes",
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
+    Item: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      noteid: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    console.error(e);
+    return failure({ status: false });
+  }
+}
